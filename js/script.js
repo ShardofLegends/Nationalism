c93074a1d522fbea545a02482335cdcb4529ba85
mabruelisauer <112378881+mabruelisauer@users.noreply.github.com>
2022-11-21 16:57:04 +0100

    Finally working search function

--- js/script.js
+++ js/script.js
@@ -15,65 +15,37 @@ let daten = await getNames();
 console.log(daten);
 
 const loadNames = () => {
-  for (let i = 0; i < 1000; i++) {
-    const html = `
-     <li class="names">
-                  <span>${daten[i]}</span>
+  displayNames(daten);
+  }
+
+  const displayNames = (Names) => {
+    const htmlString = Names
+    .map((Name) => {
+            return `
+            <li class="names">
+                  <span>${Name}</span>
                   <i class="names_delete"></i>
       </li>
-      `;
-    list.innerHTML += html;
-  }
-}
-
-
-
-//   name_input.addEventListener('keyup', (e) => {
-//     const searchString = e.target.value.toLowerCase();
-
-//     const filteredNames = daten.filter(() => {
-//         return (
-//             //personen.value.toLowerCase().includes(searchString)
-//             daten.includes(searchString)
-//         );
-//     });
-//     displayNames(filteredNames);
-//   });
-
-//   const displayNames = (Names) => {
-//     const htmlString = Names
-//         .map((Name) => {
-//             return `
-//             <li class="names">
-//                   <span>${Name}</span>
-//                   <i class="names_delete"></i>
-//       </li>
-//         `;
-//         })
-//         .join('');
-//     list.innerHTML = htmlString;
-// };
-
+        `;
+        })
+        .join('');
+    list.innerHTML = htmlString;
+};
 loadNames();
-// displayNames();
-  // const filterTodos = (term) => {
-  //   Array.from(daten.children)
-  //     .filter((show) => !show.textContent.toLowerCase().includes(term))
-  //     .forEach((show) => show.classList.add("filtered"));
-    
-    
-    
-  //   Array.from(daten.children)
-  //     .filter((search_field) => search_field.textContent.toLowerCase().includes(term))
-  //     .forEach((search_field) => search_field.classList.remove("filtered"));
-    
-  // };
 
-  // const search = document.querySelector('.search_field');
-  // search.addEventListener("keyup", () => {
-  //   const term = search.value.trim().toLowerCase();
-  //     filterTodos(term);
-  //   }); 
+  name_input.addEventListener('keyup', (e) => {
+    const searchString = e.target.value;
+
+    
+    const filteredNames = daten.filter((name) => String(name).includes(searchString))
+    console.log(searchString)
+    console.log(filteredNames)
+    displayNames(filteredNames);
+  });
+
+
+
+ 
 
 
 
