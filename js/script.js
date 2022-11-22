bea14d8b66e81a82eafd4109c4bd01a73b3455a6
mabruelisauer <112378881+mabruelisauer@users.noreply.github.com>
2022-11-22 12:49:44 +0100

    active click event for names (silvan everywhere)

--- js/script.js
+++ js/script.js
@@ -1,15 +1,13 @@
-const list = document.querySelector(".nameslist_container")
-const name_input = document.getElementById('search_field');
-const new_input = document.getElementById('new_name_field')
-
-
+const list = document.querySelector(".nameslist_container");
+const name_input = document.getElementById("search_field");
+const new_input = document.getElementById("new_name_field");
 
 async function getNames() {
   const data = await fetch("../names.json");
   const json = await data.json();
 
   return json.map((v) => v.vorname);
-  }
+}
 
 let daten = await getNames();
 
@@ -17,58 +15,67 @@ console.log(daten);
 
 const loadNames = () => {
   displayNames(daten);
-  }
+};
 
-  const displayNames = (Names) => {
-    const htmlString = Names
-    .map((Name) => {
-            return `
+const displayNames = (Names) => {
+  const htmlString = Names.map((Name) => {
+    return `
             <li id="li" class="names">
                   <span>${Name}</span>
                   <button id="edit_button" type="submit">Edit</button>
                   <button id="delete_button" type="submit">Del</button>
       </li>
         `;
-        })
-        .join('');
-    list.innerHTML = htmlString;
-    // var listItem = document.createElement("li");
-    // listItem.appendChild(edit_button);
-	  // listItem.appendChild(delete_button);
+  }).join("");
+  list.innerHTML = htmlString;
+  // var listItem = document.createElement("li");
+  // listItem.appendChild(edit_button);
+  // listItem.appendChild(delete_button);
 };
 
 loadNames();
 
-  name_input.addEventListener('keyup', (e) => {
-    const searchString = e.target.value.toLowerCase();
+name_input.addEventListener("keyup", (e) => {
+  const searchString = e.target.value.toLowerCase();
 
-    
-    const filteredNames = daten.filter((name) => String(name).toLowerCase().includes(searchString))
-    console.log(searchString)
-    console.log(filteredNames)
-    displayNames(filteredNames);
+  const filteredNames = daten.filter((name) =>
+    String(name).toLowerCase().includes(searchString)
+  );
+  displayNames(filteredNames);
+});
+
+save_button.addEventListener("click", function () {
+  const newSearchString = new_input.value;
+  daten.unshift(newSearchString);
+  displayNames(daten);
+});
+
+/* const nameSelector = document.querySelectorAll(".names");
+nameSelector.forEach((element) => {
+  element.addEventListener("click", () => {
+    console.log(element.querySelector("span").innerHTML);
   });
+}); */
 
-  save_button.addEventListener('click', function(){
-    const newSearchString = new_input.value;
-    console.log(newSearchString);
-    daten.unshift(newSearchString);
-    displayNames(daten);
-    console.log(daten);
-  })
-  
-  // delete_button.addEventListener('click', function() {
-  //   var listItem = this.parentNode;
-	// 	var ul = listItem.parentNode;
-	// 	//Remove the parent list item from the ul.
-	// 	ul.removeChild(listItem);
-  // });
+document.addEventListener("click", (e) => {
+  if(e.target?.querySelector("span")?.innerHTML != undefined){
+    console.log(e.target?.querySelector("span")?.innerHTML);
+  }
+});
 
+// apiCall(e.value)
+// function apiCall(nameToSearch){
+//   console.log(nameToSearch)
+// }
 
+// async function funcName(url){
+//   const response = await fetch(url);
+//   var data = await response.json();
+//   }
 
-
-
- 
-
-
-
+// delete_button.addEventListener('click', function() {
+//   var listItem = this.parentNode;
+// 	var ul = listItem.parentNode;
+// 	//Remove the parent list item from the ul.
+// 	ul.removeChild(listItem);
+// });
